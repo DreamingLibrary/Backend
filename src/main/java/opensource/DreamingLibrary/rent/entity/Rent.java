eba6e83b96989324f819800420f46d963bfabfff
KKdohyeong <dohyeong254@naver.com>
2025-04-01 04:03:23 +0900

    [#6] fix: rent 자료형 변경
    rentalPeriod는 빌릴 수 있는 날짜이므로 Integer로 변경

--- src/main/java/opensource/DreamingLibrary/rent/entity/Rent.java
+++ src/main/java/opensource/DreamingLibrary/rent/entity/Rent.java
@@ -3,6 +3,7 @@ package opensource.DreamingLibrary.rent.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import opensource.DreamingLibrary.book.entity.Book;
+import opensource.DreamingLibrary.global.entity.TimeStamp;
 import opensource.DreamingLibrary.group.entity.Group;
 import opensource.DreamingLibrary.user.entity.User;
 
@@ -15,14 +16,13 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Rent {
+public class Rent extends TimeStamp {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "rentId")
     private Long rentId; // PK
 
-    // ERD에 따라 userId, groupId, bookId는 각각 User, Group, Book 엔티티와 ManyToOne 관계
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId", nullable = false)
     private User user;
@@ -35,9 +35,9 @@ public class Rent {
     @JoinColumn(name = "bookId", nullable = false)
     private Book book;
 
-    // 대여 기간(시작 시점 or 기간?)이 DATETIME이라고 하셨으므로 LocalDateTime으로 매핑
+    // 대여 기간(기간)
     @Column(name = "rentalPeriod", nullable = false)
-    private LocalDateTime rentalPeriod;
+    private Integer rentalPeriod;
 
     // 반납 시각
     @Column(name = "returnAt")
@@ -46,11 +46,4 @@ public class Rent {
     // 연장 횟수
     @Column(name = "rentalCount")
     private Integer rentalCount;
-
-    // 생성/수정 시각
-    @Column(name = "createdAt", nullable = false)
-    private LocalDateTime createdAt;
-
-    @Column(name = "updatedAt", nullable = false)
-    private LocalDateTime updatedAt;
 }
