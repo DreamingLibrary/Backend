8974241e87548d938d40dbbf45048953cac9b03c
김희진 <gimijin@gimhuijin-ui-MacBookAir.local>
2025-06-04 18:37:33 +0900

    cors에러 수정

--- src/main/java/opensource/DreamingLibrary/user/config/SecurityConfig.java
+++ src/main/java/opensource/DreamingLibrary/user/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import opensource.DreamingLibrary.user.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -48,8 +49,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        http
+                .cors(Customizer.withDefaults());
         //csrf disable
-
         http
                 .csrf((auth) -> auth.disable());
 
