ff45f91e1be1ea10c6e12a02103a45a5090c9cad
김희진 <gimijin@gimhuijin-ui-MacBookAir.local>
2025-06-04 19:22:47 +0900

    join 수정

--- src/main/java/opensource/DreamingLibrary/user/controller/AuthController.java
+++ src/main/java/opensource/DreamingLibrary/user/controller/AuthController.java
@@ -1,10 +1,16 @@
 package opensource.DreamingLibrary.user.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import opensource.DreamingLibrary.user.dto.request.UserCreateRequest;
+import opensource.DreamingLibrary.user.dto.response.UserResponse;
 import opensource.DreamingLibrary.user.service.UserService;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
@@ -22,8 +28,12 @@ public class AuthController {
     //회원가입
     @PostMapping("/join")
     @Operation(summary = "회원가입")
-    public ResponseEntity<?> register(@Valid @RequestBody UserCreateRequest request) throws BadRequestException {
-        userService.register(request);
+    @ApiResponses(value={
+            @ApiResponse(responseCode="200", content = {@Content(schema= @Schema(implementation = UserResponse.class)
+            )})
+    })
+    public ResponseEntity<?> register(HttpServletResponse response, @Valid @RequestBody UserCreateRequest request) throws BadRequestException {
+        userService.register(response, request);
         return ResponseEntity.ok("회원가입성공");
     }
     @GetMapping("/")
